3b9e2aaf3c58a26d742abaccc242402166557307
Jakub Stasiak <kuba.stasiak@gmail.com>
2012-04-04 10:16:05 +1000

    Add exception catching to main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include "myapp.h"
 
+#include <cstdio>
+#include <exception>
+using namespace std;
+
 int main()
 {
-	Application* app = new MyApp();
-	int returnCode = app->run();
-	delete app;
+	int returnCode = 0;
+	Application* app = 0;
+	try {
+		app = new MyApp();
+		int returnCode = app->run();
+	}
+	catch(exception* e) {
+		printf("Exception occured: %s\n\n", e->what());
+		returnCode = -1;
+	}
+	
+	if(app) {
+		delete app;
+	}
+
 	return returnCode;
 }

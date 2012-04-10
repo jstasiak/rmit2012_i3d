edbd79b1ffa451a081cf878e01fdd2ae6556e71a
Jakub Stasiak <kuba.stasiak@gmail.com>
2012-04-11 04:04:24 +1000

    Start using Wave class to animate Water

--- src/app/water.cpp
+++ src/app/water.cpp
@@ -1,26 +1,29 @@
 #include "precompile.h"
 #include "water.h"
 
+#include "../engine/utils.h"
+
 Water::Water()
-	: size(new glm::vec2(20, 20)),
-	segments(new glm::ivec2(10, 10)) {
+	: size(20, 20),	segments(10, 10),
+	waveX((new Wave())->setAmplitude(2)->setLength(5)->setFrequency(1)),
+	waveY((new Wave())->setAmplitude(2)->setLength(7)->setFrequency(1.2f))
+{
 
 }
 
 Water::~Water() {
-	delete this->size;
-	delete this->segments;
-
-	this->size = 0;
-	this->segments = 0;
 }
 
 void Water::draw() {
+	float time = millisecondsNow() / 1000.0f;
 	auto center = glm::vec2(0, 0);
-	auto p1 = (center + glm::vec2(-0.5, -0.5) * *this->size).swizzle(glm::X, glm::Y, glm::X);
-	auto p2 = (center + glm::vec2(0.5, -0.5) * *this->size).swizzle(glm::X, glm::Y, glm::X);
-	auto p3 = (center + glm::vec2(0.5, 0.5) * *this->size).swizzle(glm::X, glm::Y, glm::X);
-	auto p4 = (center + glm::vec2(-0.5, 0.5) * *this->size).swizzle(glm::X, glm::Y, glm::X);
+	center.y += this->waveX->valueForPositionAndTime(0, time);
+	center.y += this->waveY->valueForPositionAndTime(0, time);
+
+	auto p1 = (center + glm::vec2(-0.5, -0.5) * this->size).swizzle(glm::X, glm::Y, glm::X);
+	auto p2 = (center + glm::vec2(0.5, -0.5) * this->size).swizzle(glm::X, glm::Y, glm::X);
+	auto p3 = (center + glm::vec2(0.5, 0.5) * this->size).swizzle(glm::X, glm::Y, glm::X);
+	auto p4 = (center + glm::vec2(-0.5, 0.5) * this->size).swizzle(glm::X, glm::Y, glm::X);
 
 	p1.z = 0;
 	p2.z = 0;
@@ -44,4 +47,3 @@ void Water::draw() {
 		glVertex3f(p4.x, p4.y, 0.0f);
 	glEnd();
 }
-
--- src/app/water.h
+++ src/app/water.h
@@ -1,18 +1,22 @@
+#include "precompile.h"
+
 #ifndef WATER_H
 #define WATER_H
 
-#include <glm/glm.hpp>
+#include "wave.h"
 
 class Water {
 private:
-	glm::vec2* size;
-	glm::ivec2* segments;
+	glm::vec2 size;
+	glm::ivec2 segments;
+
+	boost::scoped_ptr<Wave> waveX;
+	boost::scoped_ptr<Wave> waveY;
 
 public:
 	Water();
 	virtual ~Water();
 	void draw();
-
 };
 
 #endif // WATER_H

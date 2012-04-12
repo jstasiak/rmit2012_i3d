35fd303a89384b3c3290d85627e207d50e88438c
Jakub Stasiak <kuba.stasiak@gmail.com>
2012-04-13 03:03:03 +1000

    Set target key bindings

--- src/app/myapp.cpp
+++ src/app/myapp.cpp
@@ -27,6 +27,15 @@ void MyApp::initialize() {
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHT0);
+
+	this->bindings[SDLK_a] = "toggle_axes";
+	this->bindings[SDLK_EQUALS] = "increase_water_tesselation";
+	this->bindings[SDLK_MINUS] = "decrease_water_tesselation";
+	this->bindings[SDLK_n] = "toggle_normals";
+	this->bindings[SDLK_UP] = "+accel";
+	this->bindings[SDLK_DOWN] = "+deccel";
+	this->bindings[SDLK_LEFT] = "+left";
+	this->bindings[SDLK_RIGHT] = "+right";
 }
 
 void MyApp::update(FrameEventArgs* args) {

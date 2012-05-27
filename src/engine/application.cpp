005261a4ec3272ef2cb42264a239fc0fa2a2324d
Jakub Stasiak <kuba.stasiak@gmail.com>
2012-05-27 18:24:28 +1000

    Enable OpenGL blending by default

--- src/engine/application.cpp
+++ src/engine/application.cpp
@@ -191,6 +191,9 @@ void Application::initializeGraphics() {
 	glEnable(GL_DEPTH_TEST);
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_SCISSOR_TEST);
+
+	glEnable (GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void Application::executeConfigFile(string configFile) {
